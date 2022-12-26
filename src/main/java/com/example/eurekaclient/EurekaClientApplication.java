d73a91e6111b09655481159683945b3afb2ba5e0
livingstonantony <livingstonantony70@gmail.com>
2022-12-26 10:31:11 +0530

    initial commit

--- src/main/java/com/example/eurekaclient/EurekaClientApplication.java
+++ src/main/java/com/example/eurekaclient/EurekaClientApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Enab
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
